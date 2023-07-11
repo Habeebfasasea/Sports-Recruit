55884b368507f0458d453adbb86dff3571d9da61
Tunji Ebifemi <tunjiebifemi@gmail.com>
2023-07-11 18:52:33 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <AcademicFitReport :athlete="athlete_info"/>
+  </div>
+</template>
+
+<script>
+import AcademicFitReport from "@/components/AcademicFitReport.vue";
+import AthleteData from "@/assets/data.json";
+import './assets/tailwind.css';
+
+export default {
+  name: "app",
+  components: {
+    AcademicFitReport
+  },
+  data: function() {
+    return {
+      athlete_info: AthleteData.data[0]
+    };
+  }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './assets/main.css';
+import Vue from "vue";
+import App from "./App.vue";
+import './assets/tailwind.css'
+
+
+Vue.config.productionTip = false;
+
+new Vue({
+  el: "#app", // Binds to the #app element in public/index.html; Only works with this ID within codesandbox
+  render: h => h(App)
+});
