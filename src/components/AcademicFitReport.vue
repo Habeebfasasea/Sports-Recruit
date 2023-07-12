252bb4b4a0105d85027ee97143433bf15e71f7d4
Tunji Ebifemi <tunjiebifemi@gmail.com>
2023-07-11 22:16:22 -0400

    Create Academic Table

--- src/components/AcademicFitReport.vue
+++ src/components/AcademicFitReport.vue
@@ -1,35 +1,43 @@
 <template>
    
-  <div class="container">    
-      <div class="info">
-        
-        <h2>{{athlete.name}}</h2>
-        <ul>
-          <li>
-            <label>Sport:</label>
-            {{athlete.sport}}
-          </li>
-          <li>
-            <label>Class:</label>
-            {{athlete.grad_year}}
-          </li>
-          <li>
-            <label>Club:</label>
-            {{athlete.club.name}}
-          </li>
-          <li>
-            <label>High School:</label>
-            {{athlete.high_school.name}}
-          </li>
-          <li>
-            <label>GPA:</label>
-            {{athlete.gpa}}
-          </li>
-          <li>
-            <label>Desired Major:</label>
-            {{athlete.major}}
-          </li>
-        </ul>
+  <div class="container mx-auto">    
+      <div class="">
+        <table class="min-w-full bg-black border border-gray-200 mt-10">
+          <thead>
+            <tr>
+              <th class="text-white py-2 px-4 border-b text-sm">School Name</th>
+              <th class="text-white py-2 px-4 border-b text-sm">Athletic Div</th>
+              <th class="text-white py-2 px-4 border-b text-sm">Conference</th>
+              <th class="text-white py-2 px-4 border-b text-sm">Ranking*<p class="text-xs">(DI NCAA)</p><p class="text-xs">(DIll & Dill Hero Sports)</p></th>
+              <th class="text-white py-2 px-4 border-b text-sm">Min</th>
+              <th class="text-white py-2 px-4 border-b text-sm">25%</th>
+              <th class="text-white py-2 px-4 border-b text-sm"><p>GPA**</p> 50%</th>
+              <th class="text-white py-2 px-4 border-b text-sm">75%</th>
+              <th class="text-white py-2 px-4 border-b text-sm">Max</th>
+              <th class="text-white py-2 px-4 border-b text-sm"><p class="text-xs">SAT Reading***</p> 25%-75%</th>
+              <th class="text-white py-2 px-4 border-b text-sm"><p class="text-xs">SAT Math***</p> 25%-75%</th>
+              <th class="text-white py-2 px-4 border-b text-sm"><p class="text-xs">ACT Composite***</p> 25%-75%</th>
+            </tr>
+          </thead>
+          <tbody class="text-center">
+            <tr v-for="(report, index) in athlete.report" :key="index" class="even:bg-sky-100 odd:bg-gray-50">
+              <td class="py-2 px-4 border-b text-xs">{{ report.school }}</td>
+              <td class="py-2 px-4 border-b text-xs">{{ report.division }}</td>
+              <td class="py-2 px-4 border-b text-xs">{{ report.conference }}</td>
+              <td class="py-2 px-4 border-b text-xs">{{ report.ranking }}</td>
+              <td class="py-2 px-4 border-b text-xs">{{ report.gpa.min }}</td>
+              <td class="py-2 px-4 border-b text-xs">{{ report.gpa['25%'] }}</td>
+              <td class="py-2 px-4 border-b text-xs">{{ report.gpa['50%'] }}</td>
+              <td class="py-2 px-4 border-b text-xs">{{ report.gpa['75%'] }}</td>
+              <td class="py-2 px-4 border-b text-xs">{{ report.gpa.max }}</td>
+              <td class="py-2 px-4 border-b text-xs">{{ formatRange(report.sat.reading.min, report.sat.reading.max) }}</td>
+              <td class="py-2 px-4 border-b text-xs">Data 3</td>
+              <td class="py-2 px-4 border-b text-xs">Data 3</td>              
+            </tr>
+          </tbody>
+        </table>
+
+
       </div>   
   </div>
 </template>
@@ -42,6 +50,15 @@ export default {
       type: Object,
       required: false
     }
+  },
+  methods: {
+    formatRange(min, max) {
+      if (min === 'N/A' || max === 'N/A') {
+        return 'Not Reported';
+      }
+      return `${min}-${max}`;
+    },
+
   }
 };
 </script>
